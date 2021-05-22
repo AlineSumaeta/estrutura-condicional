6a0c757742ab52be0ba2207b0e3784932a8c174b
Aline Sumaeta <alinesumaetar@gmail.com>
2021-05-22 17:51:37 -0400

    Estrutura condicional

---
+++ src/estrutura_condicional/Main.java
@@ -0,0 +1,25 @@
+package estrutura_condicional;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner (System.in);
+		
+		int hora;
+		
+		System.out.println("Quantas horas?");
+		hora = scanner.nextInt();
+		
+		if (hora < 12) {
+			System.out.println("Bom dia!");
+		}else {
+			System.out.println("Boa tarde!");
+		}
+		
+		scanner.close();
+	}
+
+}
